7d3d02535e959f463b6dfd89ccb1326488feaed3
matsudasatsuki <yk.xxx.org@gmail.com>
2022-03-04 14:08:07 +0900

    add ABC159D

---
+++ BootCamp_medium/ABC159D.py
@@ -0,0 +1,93 @@
+from optparse import Values
+
+from numpy import mat
+
+
+def resolve():
+    N = int(input())
+    A = [int(x)for x in input().split()]
+    #print(A)
+    l = [0]*N
+    for i in range(N):
+        l[A[i]-1] += 1
+    ans = 0
+    for j in range(N):
+        ans += l[j]*(l[j]-1)//2
+    for k in range(N):
+        print(ans-l[A[k]-1]+1)
+    
+
+    
+        
+    
+    
+        
+
+
+    
+
+
+
+
+
+
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """5
+1 1 2 1 2"""
+        output = """2
+2
+3
+2
+3"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """4
+1 2 3 4"""
+        output = """0
+0
+0
+0"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """5
+3 3 3 3 3"""
+        output = """6
+6
+6
+6
+6"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """8
+1 2 1 4 2 1 4 1"""
+        output = """5
+7
+5
+7
+7
+5
+7
+5"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
