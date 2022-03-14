84542d0c683b484c77a2cc6e9cb25b36c52d5147
matsudasatsuki <yk.xxx.org@gmail.com>
2022-03-14 16:42:21 +0900

    add ABC066

---
+++ BootCamp_medium/ABC066C.py
@@ -0,0 +1,67 @@
+from collections import deque
+
+
+def resolve():
+    from collections import deque
+    n = int(input())
+    a = list(map(int,input().split()))
+    b = deque()
+    for i in range(n):
+        if i%2 == n%2:#偶奇が一致した場合
+            b.append(a[i])
+        else:
+            b.appendleft(a[i])
+    print(*b)
+
+        
+
+
+
+
+
+
+
+
+
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """4
+1 2 3 4"""
+        output = """4 2 1 3"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """3
+1 2 3"""
+        output = """3 1 2"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """1
+1000000000"""
+        output = """1000000000"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """6
+0 6 7 6 7 0"""
+        output = """0 6 6 0 7 7"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
