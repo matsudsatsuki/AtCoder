7dbe0a27e2853fd7d75e95a9b9a51a8ed470f445
matsudasatsuki <yk.xxx.org@gmail.com>
2022-03-18 15:46:53 +0900

    add AGC027

---
+++ BootCamp_easy/AGC027A.py
@@ -0,0 +1,64 @@
+
+
+def resolve():
+    N,x = map(int,input().split())
+    a = list(map(int,input().split()))
+    ans = 0
+    a.sort()
+    for num in a:
+        x -= num
+        if x >= 0:
+            ans += 1
+        else:
+            break
+    if x > 0:
+        ans -= 1
+        
+        
+    print(ans)
+        
+
+
+
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """3 70
+20 30 10"""
+        output = """2"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """3 10
+20 30 10"""
+        output = """1"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """4 1111
+1 10 100 1000"""
+        output = """4"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """2 10
+20 20"""
+        output = """0"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
